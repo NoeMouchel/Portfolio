46244fb7002d841750c25bfc1e3b5692b619581e
Noé Mouchel <noe.mouchel@gmail.com>
2025-10-20 00:42:57 +0200

    Minor update

--- portfolio-app/src/Scripts/Datas/AboutDatas.js
+++ portfolio-app/src/Scripts/Datas/AboutDatas.js
@@ -1,6 +1,6 @@
 const abouts = {
     quick_presentation: "Hello, I'm Noé!",
-    my_current_state: "I'm a student studying game programming at <b>ISART Digital Paris</b>.",
+    my_current_state: "I’m a <b>Game Programming graduate from ISART Digital Paris</b>",
     complete_description: "I have a strong passion for <b>game development</b>, <b>coding</b>, and <b>the creation of tools</b>. Exploring the world of <b>artificial intelligence</b> is another area that piques my interest. Feel free to explore my portfolio and discover the exciting projects I've been involved in. <b>Welcome!</b>"
 };
 
